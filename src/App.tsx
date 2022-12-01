50aa939a7a96ad641e2f91e111a428d1ed79dbc1
Julian Pagtama <jpagtama@gmail.com>
2022-12-01 10:06:29 -0800

    Add route and provide store to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Provider } from 'react-redux';
+import store from './store';
+import { Routes, Route } from 'react-router-dom';
+import Main from './components/Main'
+import SideNav from './components/SideNav'
 import './App.css'
 
-function App() {
+const App = () => {
+
+  const welcome = (<>
+    <SideNav /><Main />
+  </>)
 
   return (
     <div className="App">
-
+      <Provider store={store} >
+        <Routes>
+          <Route path="/" element={welcome} ></Route>
+        </Routes>
+      </Provider>
     </div>
   )
 }
