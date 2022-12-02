3ea5989d6193aed0998769d034ece234785ff9ad
Julian Pagtama <jpagtama@gmail.com>
2022-12-02 11:38:26 -0800

    Add react-event-viewer-calendar to list of projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { useInView } from 'react-intersection-observer'
 import { navActions } from '../store/navSlice'
 import flekinGif from '../assets/flekin_med.gif'
+import calendarGif from '../assets/calendar.gif'
 import styles from '../styles/Projects.module.css'
 
 const Projects = () => {
@@ -27,17 +28,33 @@ const Projects = () => {
             <div id="Projects" className={styles.headingContainer}>
                 <h1 ref={ref} className={alreadyViewed ? styles.slideIn : ''}>Projects</h1>
             </div>
-            <div className={styles.projContainer}>
-                <img className={styles.demoGif} src={flekinGif} alt="flekin" />
-                <div className={styles.projTextContainer}>
-                    <h4 className={styles.projTitle}>Flekin</h4>
-                    <div className={styles.projTech}>
-                        <span>React | JavaScript | NPM</span>
+            <div className={styles.allProjectsContainer}>
+                <div className={styles.projContainer}>
+                    <img className={styles.demoGif} src={calendarGif} alt="calendar demo" />
+                    <div className={styles.projTextContainer}>
+                        <h4 className={styles.projTitle}>react-event-viewer-calendar</h4>
+                        <div className={styles.projTech}>
+                            <span>React | TypeScript | NPM</span>
+                        </div>
+                        <p className={styles.projDesc}>react-event-viewer-calendar is an npm package that allows developers to display daily events in a monthly view. It's lightweight, responsive, and customizable.</p>
+                        <div className={styles.projLinks}>
+                            <a href="https://react-event-viewer-calendar-r34x4.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project</a>
+                            <a href="https://github.com/jpagtama/react-event-viewer-calendar" target="_blank" rel="noopener noreferrer" >View the code</a>
+                        </div>
                     </div>
-                    <p className={styles.projDesc}>Flekin is an npm package that allows developers to automatically extract the number of words, syllables, and sentences. Returns a readability score as processed by the Flesch-Kincaid formula.</p>
-                    <div className={styles.projLinks}>
-                        <a href="https://flekin-7qyrt.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project</a>
-                        <a href="https://github.com/jpagtama/flekin" target="_blank" rel="noopener noreferrer" >View the code</a>
+                </div>
+                <div className={styles.projContainer}>
+                    <img className={styles.demoGif} src={flekinGif} alt="flekin demo" />
+                    <div className={styles.projTextContainer}>
+                        <h4 className={styles.projTitle}>Flekin</h4>
+                        <div className={styles.projTech}>
+                            <span>React | JavaScript | NPM</span>
+                        </div>
+                        <p className={styles.projDesc}>Flekin is an npm package that allows developers to automatically extract the number of words, syllables, and sentences. Returns a readability score as processed by the Flesch-Kincaid formula.</p>
+                        <div className={styles.projLinks}>
+                            <a href="https://flekin-7qyrt.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project</a>
+                            <a href="https://github.com/jpagtama/flekin" target="_blank" rel="noopener noreferrer" >View the code</a>
+                        </div>
                     </div>
                 </div>
             </div>
