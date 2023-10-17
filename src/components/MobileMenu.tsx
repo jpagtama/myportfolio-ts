889c18fbe65b114987405e8cd6e6055c76ad3aba
Julian Pagtama <jpagtama@gmail.com>
2023-10-17 11:12:02 -0700

    Add MobileMenu using menuItems as props

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import MenuItem from './MenuItem';
 
-const MobileMenu = () => {
+interface Selector {
+    nav: { active: string }
+}
+
+interface Props {
+    menuItems: string[]
+}
+
+const MobileMenu = ({ menuItems }: Props) => {
+
+    const activeLabel = useSelector((state: Selector) => state.nav.active)
+    const menuSections = menuItems.map((item, i) => {
+        const isActive = item.toLowerCase() === activeLabel.toLowerCase()
+        return <MenuItem key={i} title={item} isActive={isActive} />
+    });
+
     return (
         <nav className='flex flex-col h-full w-full'>
-
-
+            <ul className='flex flex-col gap-8'>
+                {menuSections}
+            </ul>
         </nav>
     )
 }
