056ff8ca4cb5727a701250984b1f991e3f17b9ad
Julian Pagtama <jpagtama@gmail.com>
2023-10-17 09:13:08 -0700

    Add initial MobileMenu nav component

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MobileMenu = () => {
+    return (
+        <nav className='flex flex-col h-full w-full'>
+
+
+        </nav>
+    )
+}
+
+export default MobileMenu
