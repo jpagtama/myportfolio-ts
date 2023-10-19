1434b62906a406807040aced13d85a51ab32d386
Julian Pagtama <jpagtama@gmail.com>
2023-10-19 08:50:13 -0700

    Conditionally display mobile/desktop nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,7 +3,7 @@ import Menu from './Menu';
 import MobileMenu from './MobileMenu';
 import { useSelector } from 'react-redux';
 import MenuButton from './MenuButton';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence, useScroll, useMotionValueEvent } from 'framer-motion';
 
 interface Selector {
     nav: { showMenu: boolean }
@@ -14,21 +14,26 @@ const menuItems = ['About', 'Experience', 'Projects', 'Skills', 'Education', 'In
 const SideNav = () => {
     const { showMenu } = useSelector((state: Selector) => state.nav);
 
+
     return (
         <nav className='flex justify-center w-full fixed top-0 z-10' >
             <MenuButton />
+
+            {/* Use for mobile view */}
             <AnimatePresence>
                 {showMenu && (
-                    <motion.div className={`flex flex-col sm:hidden w-full h-screen py-8 px-4 bg-red-500`}
+                    <motion.div className={`flex flex-col sm:hidden w-full h-screen py-8 px-4 bg-indigo-300`}
                         initial={{ y: '-100%' }}
                         animate={{ y: 0 }}
-                        transition={{ duration: 0.4 }}
+                        transition={{ duration: 0.3 }}
                         exit={{ y: '-100%' }}
                     >
                         <MobileMenu menuItems={menuItems} />
                     </motion.div>
                 )}
             </AnimatePresence>
+
+            {/* Use for viewports larger than mobile */}
             <Menu menuItems={menuItems} />
         </nav>
     )
