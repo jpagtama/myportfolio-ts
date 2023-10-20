e2da465e355c67b8e0833e6ba9cbf58877584404
Julian Pagtama <jpagtama@gmail.com>
2023-10-20 11:57:35 -0700

    Implement staggered animation on menu items in mobile view

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,24 +1,35 @@
-import { useDispatch } from 'react-redux'
-import { navActions } from '../store/navSlice'
-import styles from '../styles/MenuItem.module.css'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { navActions } from '../store/navSlice';
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
 
 interface Props {
     title: string
     isActive: boolean
+    index?: number
 }
 
 const MenuItem = (props: Props) => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const clickHandler = () => {
-        dispatch(navActions.scrollTo(props.title))
-        dispatch(navActions.openMenu(false))
+        dispatch(navActions.scrollTo(props.title));
+        dispatch(navActions.openMenu(false));
     }
 
     return (
-        <li className={`${styles.listItem}`} onClick={clickHandler}>
-            <span className='sm:text-3xl'>{props.title}</span>
-        </li>
+        <motion.li onClick={clickHandler}
+            className='text-4xl sm:text-xl md:text-3xl cursor-pointer'
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            whileHover={{ scale: 1.10 }}
+            transition={{ style: 'spring', duration: 0.25, delay: props.index ? props.index * 0.1 : 0, whileHover: { duration: 0.1 } }}
+        >
+            <motion.span
+            >
+                {props.title}
+            </motion.span>
+        </motion.li>
     )
 }
 
