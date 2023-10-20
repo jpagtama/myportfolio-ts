cfde435e75d4fcb6b411f76a28c4552d34c5d329
Julian Pagtama <jpagtama@gmail.com>
2023-10-20 11:56:38 -0700

    Change look of mobile menu button

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -21,18 +21,18 @@ const MenuButton = () => {
     }
 
     return (
-        <button onClick={clickHandler} className='flex flex-col justify-center items-center self-end gap-1 rounded-full h-12 w-12 bg-indigo-300 visible sm:hidden border-none fixed top-0 right-0 mt-4 mr-4 focus:outline-none z-20'>
-            <motion.span id='topLine' className='w-7 h-1 rounded-full bg-white'
+        <button onClick={clickHandler} className='flex flex-col justify-center items-center self-end gap-1 rounded-full h-12 w-12 bg-lime-900 visible sm:hidden border-none fixed top-0 right-0 mt-4 mr-4 focus:outline-none z-20'>
+            <motion.span id='topLine' className='w-7 h-1 rounded-full bg-slate-200'
                 animate={showMenu ? 'animateTopToX' : 'animateToHamburger'}
                 variants={variants}
             >
             </motion.span>
-            <motion.span id='midLine' className='w-7 h-1 rounded-full bg-white'
+            <motion.span id='midLine' className='w-7 h-1 rounded-full bg-slate-200'
                 animate={{ opacity: showMenu ? 0 : 1, scaleX: showMenu ? 0 : 1 }}
                 transition={{ style: 'spring' }}
             >
             </motion.span>
-            <motion.span id='bottomLline' className='w-7 h-1 rounded-full bg-white'
+            <motion.span id='bottomLline' className='w-7 h-1 rounded-full bg-slate-200'
                 animate={showMenu ? 'animateBottomToX' : 'animateToHamburger'}
                 variants={variants}
             >
