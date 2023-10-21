f30375cfac1becdf43bc601d0b06cf0bdfaa81df
Julian Pagtama <jpagtama@gmail.com>
2023-10-21 12:13:20 -0700

    Remove previous styling of Experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { navActions } from '../store/navSlice'
 import { useInView } from 'react-intersection-observer'
-import styles from '../styles/Experience.module.css'
+import { motion } from 'framer-motion'
 
 const Experience = () => {
     const dispatch = useDispatch()
@@ -31,32 +31,37 @@ const Experience = () => {
     }, [expInView, alreadyViewed])
 
     return (
-        <>
-            <div id="Experience" className={styles.headingContainer} >
-                <h1 ref={expRef} className={`${styles.heading} ${alreadyViewed ? styles.slideIn : ''}`}>Experience</h1>
-            </div>
-            <div ref={firstSummaryRef} className={`${styles.container} ${styles.summary} ${firstSummaryInView ? styles.firstSummary : ''}`}>
-                <span className={styles.workDetails}>
+        <div className='flex flex-col justify-center items-center gap-12 bg-dark sm:gap-24 w-full min-h-screen py-8'>
+            <motion.h1 className='text-5xl md:text-9xl text-center px-2 font-audiowide bg-gradient-to-b from-slate-100 to-slate-900 text-transparent bg-clip-text'
+                initial={{ opacity: 0.0, x: '-100px' }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.3 }}
+                viewport={{ once: true }}
+            >
+                Work Experience
+            </motion.h1>
+            <div className='' >
+                <span >
                     <h2>Software Developer</h2>
                     <h3>Regal Medical Group</h3>
-                    <p className={styles.workDesc} >
+                    <p className='' >
                         Developed and maintained applications for one of the largest physician-led healthcare networks in Southern California. Applications used among thousands of doctors and patients across hospitals and urgent care centers.
                     </p>
                 </span>
-                <p className={styles.workDates} >January 2018 - March 2021</p>
+                <p className='' >January 2018 - March 2021</p>
             </div>
-            <div ref={secondSummaryRef} className={`${styles.container} ${secondSummaryInView ? styles.secondSummary : ''}`}>
-                <span className={styles.workDetails}>
+            <div className=''>
+                <span className=''>
                     <h2>Web Developer</h2>
                     <h3>Nonfat Media (Breakdown Services)</h3>
-                    <p className={styles.workDesc} >
+                    <p className='' >
                         Worked within a team to maintain and engineer a system of applications recognized by the Casting Society of America as the primary distributor of casting breakdowns. Applications used to service casting directors, talent representatives, and actors within a seamless, casting ecosystem.
                     </p>
                 </span>
 
-                <p className={styles.workDates} >June 2014 - January 2018</p>
+                <p className='' >June 2014 - January 2018</p>
             </div>
-        </>
+        </div>
     )
 }
 
