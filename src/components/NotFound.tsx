f793d4cf2e18d2c7cc8b933e2cb4f4b371472dd2
Julian Pagtama <jpagtama@gmail.com>
2023-10-24 16:36:44 -0700

    Remove NotFound css module and modify 404 to use tailwind styles

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,24 +1,16 @@
 import notFoundIcon from '../assets/icon404.svg'
-import styles from '../styles/NotFound.module.css'
 
 const NotFound = () => {
     return (
-        <div className={styles.container} >
-            <div className={styles.section} >
-                <div className={styles.sectionContent}>
-                    <div className={styles.descContainer}>
-                        <h1>Page not found</h1>
-                        <p className={styles.desc}>It seems this page does not exist. You can find my <a className={styles.link} href="/" >homepage here</a></p>
-                    </div>
-                </div>
+        <div className='flex flex-col sm:flex-row justify-center items-center w-screen h-screen gap-8' >
+            <div className='flex justify-center items-center bg-zinc-950 w-full h-1/2 sm:h-screen'>
+                <img className='h-72 w-72' src={notFoundIcon} alt="Page not found icon" />
             </div>
-            <div className={styles.section404}>
-                <div className={styles.sectionContent}>
-                    <img className={styles.notFoundIcon} src={notFoundIcon} alt="Page not found icon" />
-                </div>
+            <div className='flex flex-col justify-center items-center w-full h-1/2'>
+                <h1 className='text-6xl text-center'>Page not found</h1>
+                <p className='text-xl text-center'>It seems this page does not exist. You can find my <a className='text-lime-600 hover:text-lime-500' href="/" >homepage here</a></p>
             </div>
         </div>
-
     )
 }
 
