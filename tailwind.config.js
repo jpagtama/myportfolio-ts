ffd64a1410e5ee06be23b1a6c607096fbbc81c6f
Julian Pagtama <jpagtama@gmail.com>
2023-10-24 12:30:05 -0700

    Add new animation to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ export default {
         charcoal: '#121212',
         'charcoal-light': '#151515',
         accent: '#a5b4fc'
-
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" }
+        }
+      },
+      animation: {
+        wiggle: "wiggle 200ms ease-in-out"
       }
     },
   },
