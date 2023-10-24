c7de42c93534a978ec0258fed2ede7d0dd51f624
Julian Pagtama <jpagtama@gmail.com>
2023-10-24 12:29:47 -0700

    Recreate Footer with copy to clipboard button for email, adding tailwind icon, and centered layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,57 @@
-import { FaReact, FaNode, FaJs } from 'react-icons/fa'
-import { SiTypescript, SiVite } from 'react-icons/si';
-import { IconContext } from 'react-icons/lib'
-import styles from '../styles/Footer.module.css'
+import { useState, useEffect } from 'react';
+import { FaReact } from 'react-icons/fa';
+import { SiTypescript, SiVite, SiTailwindcss } from 'react-icons/si';
+import { IconContext } from 'react-icons/lib';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
-    const year = new Date().getFullYear()
+    const [wiggle, setWiggle] = useState(false);
+    const [emailCopied, setEmailCopied] = useState(false);
+
+    useEffect(() => {
+        if (emailCopied) {
+            setTimeout(() => {
+                setEmailCopied(false);
+            }, 1000);
+        }
+    }, [emailCopied]);
+
+    const year = new Date().getFullYear();
+
+    const copyToClipboard = async () => {
+        await navigator.clipboard.writeText('jpagtama@gmail.com');
+        setWiggle(true);
+        setEmailCopied(true);
+    }
 
     return (
-        <div className={styles.footerContainer}>
-            <IconContext.Provider value={{ color: "gainsboro", size: "2rem" }}>
-                <div style={{ display: "flex", gap: "1.5rem", marginBottom: "1.5rem" }}>
-                    <SiTypescript />
-                    <SiVite />
-                    <FaReact />
-                    <FaNode />
-                    <FaJs />
-                </div>
-            </IconContext.Provider>
-            <p className={styles.sideNote}>Contact me at jpagtama@gmail.com</p>
-            <p className={styles.sideNote}>Julian Pagtama &copy; {year}</p>
+        <div className='flex flex-col justify-center items-center gap-16 min-h-[75vh] p-4 bg-gradient-to-b from-zinc-950 to-black text-slate-300 text-center' >
+            <div className='flex flex-col justify-center items-center gap-6'>
+                <p className='text-4xl text-center'>Let's work together.</p>
+                <p>Contact me at:
+                    <button className={`hover:text-slate-100 hover:bg-lime-900 transition duration-300 outline-none ring-0 mx-2 w-48 ${wiggle ? 'animate-wiggle' : ''}`}
+                        onClick={copyToClipboard}
+                        onAnimationEnd={() => setWiggle(false)}
+                    >
+                        {emailCopied ?
+                            <span className='text-lime-500'>copied!</span> :
+                            <motion.span initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5 }} >jpagtama@gmail.com</motion.span>
+                        }
+                    </button>
+                </p>
+            </div>
+            <div className='flex flex-col justify-center items-center gap-4 text-slate-600'>
+                <span>Created using these technologies:</span>
+                <IconContext.Provider value={{ size: "3rem" }}>
+                    <div style={{ display: "flex", gap: "1.5rem" }}>
+                        <SiVite />
+                        <FaReact />
+                        <SiTypescript />
+                        <SiTailwindcss />
+                    </div>
+                </IconContext.Provider>
+            </div>
+            <p className='text-slate-600'>Julian Pagtama &copy; {year}</p>
         </div>
     )
 }
