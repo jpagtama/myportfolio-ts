f7e9333d46e0fbecd2553326954d7965a284b531
Julian Pagtama <jpagtama@gmail.com>
2023-10-28 18:51:35 -0700

    Wire up the themeSlice to the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import navSlice from './navSlice'
+import themeSlice from './themeSlice';
 
 const store = configureStore({
     reducer: {
-        nav: navSlice.reducer
+        nav: navSlice.reducer,
+        theme: themeSlice.reducer
     }
 })
 
 export type RootState = ReturnType<typeof store.getState>;
-export default store
+export default store;
