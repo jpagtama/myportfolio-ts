0c8b11b96744c09fbeaeac964c53bb47763d8a92
Julian Pagtama <jpagtama@gmail.com>
2023-11-30 14:46:52 -0800

    Add gap to project images and change button from go to project to go to project demo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import { navActions } from '../store/navSlice';
 import flekinGif from '../assets/flekin_med.gif';
 import calendarGif from '../assets/calendar.gif';
-import finflexiGif from '../assets/finflexi.gif';
+import finflexiProjectImg from '../assets/finflexi_project_image.webp';
 import { motion } from 'framer-motion';
 
 const Projects = () => {
@@ -27,14 +27,14 @@ const Projects = () => {
             >
                 Projects
             </motion.h1>
-            <div className='flex justify-center flex-wrap'>
+            <div className='flex justify-center flex-wrap gap-2'>
                 <motion.div className='relative'
                     initial={{ opacity: 0.15, y: 50 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ style: 'spring', duration: 0.7 }}
                     viewport={{ once: true }}
                 >
-                    <img className='' src={finflexiGif} alt="finflexi demo" />
+                    <img className='' src={finflexiProjectImg} alt="finflexi project" />
                     <div className='flex flex-col justify-around items-center max-w-[400px] w-full max-h-[300px] h-full p-4 bg-gradient-to-bl from-slate-700 to-lime-900 absolute top-0 opacity-0 hover:opacity-100 duration-200'>
                         <motion.div className='flex flex-col justify-center items-center'
                             initial={{ y: 100, opacity: 0 }}
@@ -45,8 +45,8 @@ const Projects = () => {
                         </motion.div>
                         {/* <p >Finflexi is a fintech app that allows you to view market and company data.</p> */}
                         <div className='flex gap-4'>
-                            <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://finflexi.vercel.app" target="_blank" rel="noopener noreferrer" >Go to project</a>
                             <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://github.com/jpagtama/finflexi" target="_blank" rel="noopener noreferrer" >View the code</a>
+                            <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://finflexi.vercel.app" target="_blank" rel="noopener noreferrer" >Go to project demo</a>
                         </div>
                     </div>
                 </motion.div>
@@ -67,8 +67,8 @@ const Projects = () => {
                         </motion.div>
                         {/* <p>react-event-viewer-calendar is an npm package that allows developers to display daily events in a monthly view. It's lightweight, responsive, and customizable.</p> */}
                         <div className='flex gap-4'>
-                            <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://react-event-viewer-calendar-r34x4.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project</a>
                             <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://github.com/jpagtama/react-event-viewer-calendar" target="_blank" rel="noopener noreferrer" >View the code</a>
+                            <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://react-event-viewer-calendar-r34x4.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project demo</a>
                         </div>
                     </div>
                 </motion.div>
@@ -89,8 +89,8 @@ const Projects = () => {
                         </motion.div>
                         {/* <p >Flekin is an npm package that allows developers to automatically extract the number of words, syllables, and sentences. Returns a readability score as processed by the Flesch-Kincaid formula.</p> */}
                         <div className='flex gap-4'>
-                            <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://flekin-7qyrt.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project</a>
                             <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://github.com/jpagtama/flekin" target="_blank" rel="noopener noreferrer" >View the code</a>
+                            <a className='py-2 px-2 bg-slate-300 rounded-lg text-zinc-950 hover:scale-105 hover:bg-slate-400 duration-200' href="https://flekin-7qyrt.ondigitalocean.app/" target="_blank" rel="noopener noreferrer" >Go to project demo</a>
                         </div>
                     </div>
                 </motion.div>
